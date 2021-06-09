9e193c9268050a21db518ea7295d1b096aba6b3d
lishli <lisha.li@frozenmountain.com>
2021-06-08 17:37:44 -0700

    Updated according to code polishing

--- ios/HelloWorld/HelloWorld/HelloWorldLogic.swift
+++ ios/HelloWorld/HelloWorld/HelloWorldLogic.swift
@@ -4,9 +4,20 @@ import Foundation
 class HelloWorldLogic : NSObject {
     static let instance = HelloWorldLogic()
 
-    var applicationId: String = Config.applicationId
-    var channelId: String = Config.channelId
-    var gatewayUrl: String = Config.gatewayUrl
-    var sharedSecret: String = Config.sharedSecret
+    let _applicationId: String = Config.applicationId
+        let _channelId: String = Config.channelId
+        let _gatewayUrl: String = Config.gatewayUrl
+        let _sharedSecret: String = Config.sharedSecret
+        
+        func generateToken(claims: NSMutableArray) -> String {
+            return FMLiveSwitchToken.generateClientRegister(
+                withApplicationId: _applicationId,
+                userId: (_client?.userId())!,
+                deviceId: (_client?.deviceId())!,
+                clientId: _client?.id(),
+                clientRoles: nil,
+                channelClaims: claims,
+                sharedSecret: _sharedSecret)
+        }
     
 }
