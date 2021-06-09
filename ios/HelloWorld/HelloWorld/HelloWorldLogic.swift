557b506fbf1356c8041896466d5bdef12f46a88b
lishli <lisha.li@frozenmountain.com>
2021-06-08 17:44:06 -0700

    Fixed intentation

--- ios/HelloWorld/HelloWorld/HelloWorldLogic.swift
+++ ios/HelloWorld/HelloWorld/HelloWorldLogic.swift
@@ -5,19 +5,19 @@ class HelloWorldLogic : NSObject {
     static let instance = HelloWorldLogic()
 
     let _applicationId: String = Config.applicationId
-        let _channelId: String = Config.channelId
-        let _gatewayUrl: String = Config.gatewayUrl
-        let _sharedSecret: String = Config.sharedSecret
-        
-        func generateToken(claims: NSMutableArray) -> String {
-            return FMLiveSwitchToken.generateClientRegister(
-                withApplicationId: _applicationId,
-                userId: (_client?.userId())!,
-                deviceId: (_client?.deviceId())!,
-                clientId: _client?.id(),
-                clientRoles: nil,
-                channelClaims: claims,
-                sharedSecret: _sharedSecret)
-        }
+    let _channelId: String = Config.channelId
+    let _gatewayUrl: String = Config.gatewayUrl
+    let _sharedSecret: String = Config.sharedSecret
+    
+    func generateToken(claims: NSMutableArray) -> String {
+        return FMLiveSwitchToken.generateClientRegister(
+            withApplicationId: _applicationId,
+            userId: (_client?.userId())!,
+            deviceId: (_client?.deviceId())!,
+            clientId: _client?.id(),
+            clientRoles: nil,
+            channelClaims: claims,
+            sharedSecret: _sharedSecret)
+    }
     
 }
